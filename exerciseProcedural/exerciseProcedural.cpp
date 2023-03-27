7db9aa24c7bc724c5d80c10213d9d6be9f07ec7e
nur hikmah <nur.hikmah190504@gmail.com>
2023-03-27 15:13:12 +0700

    include iostream

--- exerciseProcedural/exerciseProcedural.cpp
+++ exerciseProcedural/exerciseProcedural.cpp
@@ -1,20 +1,28 @@
-// exerciseProcedural.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
-}
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+    
+int main() {
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    double nilM, nilB;
+    cout << "masukan nilai matematika : ";
+    cin >> nilM;
+    cout << "masukan nilai bahasa inggris : ";
+    cin >> nilB;
+    // rerata = (nilaiM + nilaiB) /2;
+    // rata = rerata (nilaiM, nilaiB);
+    cout << "status Asroni diterima, Lia diterima, joko ditolak"
+
+        int main() {
+                  matematika_nilai = pendaftar "matematika"
+                      inggring_score = pendafttar "inggris"
+
+                      if avg_score >= 70 or math_score > 80:
+                  pendaftar[status] = "diterima"
+                      diterima += 1
+                      else:
+                  pendaftar[status] = "ditolak"
+                      ditolak += 1
+                      // display total kadinat dan kadinat yang tidak di terima 
+              }
